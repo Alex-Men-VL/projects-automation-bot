168386c5d8f8c95b6896b5665a5f40a0ea02e546
Aleksandr <menshikov.a.v@icloud.com>
2022-01-26 20:20:47 +0300

    Register models in admin panel

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import (
+    Student,
+    StudentLevel,
+    ProductManager,
+    Time,
+    Project,
+    Participant,
+    Team
+)
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    exclude = ('bot_state',)
+
+
+@admin.register(StudentLevel)
+class StudentLevelAdmin(admin.ModelAdmin):
+    pass
+
+
+class TimeInline(admin.TabularInline):
+    model = Time
+
+
+@admin.register(ProductManager)
+class ProductManagerAdmin(admin.ModelAdmin):
+    exclude = ('bot_state',)
+    inlines = (TimeInline,)
+
+
+@admin.register(Time)
+class TimeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    pass
+
+
+class ParticipantInline(admin.TabularInline):
+    model = Participant
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    inlines = (ParticipantInline,)
