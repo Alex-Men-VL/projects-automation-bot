9c9c2acbb7ad61ad5e3ed3132fa9ae9106bd3acf
Anna <44573387+annfike@users.noreply.github.com>
2022-01-27 14:00:47 +0300

    download data to DB from JSON

--- projects_automation_bot/settings.py
+++ projects_automation_bot/settings.py
@@ -42,9 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'django_extensions',
-
     'telegram_bot.apps.TelegramBotConfig',
 ]
 
