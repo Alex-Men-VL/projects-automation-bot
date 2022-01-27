d2a95d97793d400ba76a6b911af6993b1582d9df
itcosplay <azure_dream@mail.ru>
2022-01-27 16:23:47 +0300

    simple creating groups

--- telegram_bot/management/commands/sort.py
+++ telegram_bot/management/commands/sort.py
@@ -1,15 +1,22 @@
 import logging
+# from os import getgrouplist
 from django.core.management import BaseCommand
 from telegram_bot.models import Participant, Student, ProductManager, StudentLevel, Time
-from datetime import datetime
+# from datetime import datetime
 
     
 class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
             students = sort()
-            for student in students:
-                print(student)
+            # for student in students:
+            #     print(student)
+            groups = create_studets_groups(students)
+            for group in groups:
+                for student in group:
+                    print(student)
+                print('----')
+
         except Exception as error:
             logging.error(error)
 
@@ -38,5 +45,49 @@ def sort():
 
     return students
 
-# def get_beginners():
-# std = Participant.objects.filter(student__pk=1)
+
+def create_studets_groups(students):
+    single_users = []
+    group = []
+    groups = []
+    time = None
+
+    for student in students:
+        if len(group) == 0:
+            group.append(student)
+            time = student['time'][0]
+
+        elif len(group) == 1 and time in student['time']:
+            group.append(student)
+
+        elif len(group) == 1 and time not in student['time']:
+            single_users.append(student)
+            group = []
+            time = None
+
+        elif len(group) == 2 and time in student['time']:
+            group.append(student)
+            groups.append(group)  
+            group = []
+            time = None
+
+        elif len(group) == 2 and time not in student['time']:
+            group.append(student)
+            groups.append(group)
+            group = []
+            time = None
+        else:
+            pass
+
+    return groups
+
+
+# {'name': 'Student_1', 'id': 1, 'level': 'beginner', 'time': ['1900']}
+# {'name': 'Student_3', 'id': 3, 'level': 'beginner', 'time': ['1900']}
+# {'name': 'Student_5', 'id': 5, 'level': 'beginner', 'time': ['1900']}
+# {'name': 'Student_7', 'id': 7, 'level': 'beginner', 'time': ['1930']}
+# {'name': 'Student_8', 'id': 8, 'level': 'beginner', 'time': ['1930']}
+# {'name': 'Student_9', 'id': 9, 'level': 'beginner', 'time': ['1930']}
+# {'name': 'Student_6', 'id': 6, 'level': 'junior', 'time': ['1830']}
+# {'name': 'Student_2', 'id': 2, 'level': 'junior', 'time': ['1830', '1900']}
+# {'name': 'Student_4', 'id': 4, 'level': 'junior', 'time': ['1830', '1900', '1930']}
