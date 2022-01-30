b2befe4bfb57a3dc043e205f1ed968323c3412d7
Aleksandr <menshikov.a.v@icloud.com>
2022-01-30 19:09:20 +0300

    Add custom filters

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin, messages
+from django.contrib.admin import SimpleListFilter
+from django.db.models import Count
 
 from .collect_teams import sort_and_create_teams
 from .models import (Participant, ProductManager, Project, Student,
@@ -8,7 +10,6 @@ from .models import (Participant, ProductManager, Project, Student,
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     exclude = ('bot_state', 'chat_id')
-    readonly_fields = ('tg_username',)
 
 
 @admin.register(StudentLevel)
@@ -23,7 +24,6 @@ class TimeInline(admin.TabularInline):
 @admin.register(ProductManager)
 class ProductManagerAdmin(admin.ModelAdmin):
     exclude = ('bot_state', 'chat_id')
-    readonly_fields = ('tg_username',)
     inlines = (TimeInline,)
 
 
@@ -34,11 +34,29 @@ class TimeAdmin(admin.ModelAdmin):
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ('workspace_id',)
+
+
+class TeamExistenceFilter(SimpleListFilter):
+    title = 'Участник в команде'
+    parameter_name = 'team'
+
+    def lookups(self, request, model_admin):
+        return [
+            ("in_team", "В команде"),
+            ("not_in_team", "Не в команде"),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "in_team":
+            return queryset.filter(team__isnull=False)
+        if self.value():
+            return queryset.distinct().filter(team__isnull=True)
 
 
 @admin.register(Participant)
 class ParticipantAdmin(admin.ModelAdmin):
+    list_filter = (TeamExistenceFilter,)
     actions = ('create_teams',)
 
     @admin.action(description='Сформировать команды из участников')
@@ -57,8 +75,28 @@ class ParticipantInline(admin.TabularInline):
     model = Participant
 
 
+class ParticipantCountFilter(SimpleListFilter):
+    title = 'Количество участников'
+    parameter_name = 'participant'
+
+    def lookups(self, request, model_admin):
+        return [
+            ("three", "Трое"),
+            ("two", "Двое"),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "three":
+            return queryset.annotate(
+                participants_count=Count('participants')
+            ).filter(participants_count__exact=3)
+        if self.value():
+            return queryset.annotate(
+                participants_count=Count('participants')
+            ).filter(participants_count__exact=2)
+
+
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
     inlines = (ParticipantInline,)
-
-
+    list_filter = (ParticipantCountFilter,)
